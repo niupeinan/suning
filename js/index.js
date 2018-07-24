d61ff61a7a3bb9d73fa112b26a5b4462a4f919fa
Your Name <867761374@qq.com>
2018-07-24 08:29:53 +0800

    2

--- js/index.js
+++ js/index.js
@@ -14,4 +14,46 @@ window.onload=function () {
             rightnav[i].style.display="none";
         }
     } 
+    //轮播图展示
+    let shop=document.getElementsByClassName("shoplist")[0];
+    let bigtu=shop.getElementsByClassName("bigtu")[0];
+    let wraper=bigtu.getElementsByClassName("wraper")[0];
+    let lis=wraper.getElementsByTagName("a");
+    let back=bigtu.getElementsByClassName("left")[0];
+    let forward=bigtu.getElementsByClassName("right")[0];
+    console.log(shop,bigtu,wraper,lis,back,forward);
+    let num=0;
+    let t=setInterval(move,2000);
+    function move(){
+        num++;
+        if(num==lis.length){
+            num=0;
+        }
+        for(let j=0;j<lis.length;j++){
+            lis[j].style.zIndex=5;
+        }
+        lis[num].style.zIndex=10;
+    }
+    function move1(){
+        num--;
+        if(num<0){
+            num=lis.length-1;
+        }
+        for(let j=0;j<lis.length;j++){
+            lis[j].style.zIndex=5;
+        }
+        lis[num].style.zIndex=10;
+    }
+    wraper.onmouseenter=function(){
+        clearInterval(t);
+    }
+    wraper.onmouseleave=function(){
+        t=setInterval(move,2000);
+    }
+    back.onclick=function(){
+        move1();
+    }
+    forward.onclick=function(){
+        move();
+    }
 }
